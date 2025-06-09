908a793a0de410f275598d06731ba88d00bea6b3
taeyeon0319 <xodus1349@likelion.org>
2025-06-09 21:46:03 +0900

    Feat: 관리자 컴포넌트 작업2차

--- src/component/administrator/Pickup/PickupBox.jsx
+++ src/component/administrator/Pickup/PickupBox.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Search from '../../common/Search/Search';
+import PickupItem from './PickupItem';
 import Pagination from '../../common/Pagination/Pagination';
 import styles from './Pickup.module.css';
 
@@ -8,58 +9,79 @@ const dummyProducts = [
   {
     id: 1,
     image: 'https://cdn.hellodd.com/news/photo/201909/69577_craw1.jpg',
-    name: '상품 A',
-    price: 10000,
+    brand: '브랜드 A',
+    category: '카테고리 A',
+    grade: 'S',
+    point: 10000,
     date: '2025-06-01',
+    state: 'REGISTER',
   },
   {
     id: 2,
     image: 'https://flexible.img.hani.co.kr/flexible/normal/970/710/imgdb/original/2022/0107/20220107501703.jpg',
-    name: '상품 B',
-    price: 12000,
+    brand: '브랜드 B',
+    category: '카테고리 B',
+    grade: 'A',
+    point: 13000,
     date: '2025-06-02',
+    state: 'FINISHED',
   },
   {
     id: 3,
     image: '',
-    name: '상품 C',
-    price: 13000,
+    brand: '브랜드 C',
+    category: '카테고리 C',
+    grade: 'A',
+    point: 11000,
     date: '2025-06-03',
+    state: 'FIRST_INSPECTED',
   },
   {
     id: 4,
     image: '',
-    name: '상품 D',
-    price: 15000,
-    date: '2025-06-04',
+    brand: '브랜드 D',
+    category: '카테고리 D',
+    grade: 'S',
+    point: 15000,
+    date: '2025-06-01',
+    state: 'FIRST_INSPECTED',
   },
   {
     id: 5,
     image: '',
-    name: '상품 E',
-    price: 16000,
-    date: '2025-06-05',
+    brand: '브랜드 E',
+    category: '카테고리 E',
+    grade: 'S',
+    point: 10000,
+    date: '2025-06-01',
+    state: 'Register',
   },
   {
     id: 6,
     image: '',
-    name: '상품 F',
-    price: 17000,
-    date: '2025-06-06',
+    brand: '브랜드 F',
+    category: '카테고리 F',
+    grade: 'C',
+    point: 1000,
+    date: '2025-06-01',
+    state: 'DELIVERING',
   },
   {
     id: 7,
     image: '',
-    name: '상품 G',
-    price: 11000,
-    date: '2025-06-06',
+    brand: '브랜드 G',
+    category: '카테고리 G',
+    grade: 'S',
+    point: 20000,
+    date: '2025-06-01',
+    state: 'Register',
   },
   
 ];
 
 const PickupBox = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 6;
+  const itemsPerPage = 5;
 
   const indexOfLast = currentPage * itemsPerPage;
   const indexOfFirst = indexOfLast - itemsPerPage;
@@ -68,7 +90,7 @@ const PickupBox = () => {
   const totalPages = Math.ceil(dummyProducts.length / itemsPerPage);
 
   return (
-    <div className={styles.productBox}>
+    <div className={styles.pickupBox}>
       <div className={styles.header}>
         <div className={styles.leftHeader}>
           <div className={styles.tag} />
@@ -77,11 +99,24 @@ const PickupBox = () => {
         <Search/>
       </div>
 
-      {/* <div className={styles.grid}>
-        {currentItems.map((product) => (
-          <ProductItem key={product.id} {...product} />
-        ))}
-      </div> */}
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>상품 이미지</th>
+            <th>브랜드</th>
+            <th>카테고리</th>
+            <th>등급</th>
+            <th>포인트</th>
+            <th>날짜</th>
+            <th>진행 상태</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((product) => (
+            <PickupItem key={product.id} product={product} />
+          ))}
+        </tbody>
+      </table>
 
        <Pagination
         currentPage={currentPage}
---
+++ src/component/administrator/UserList/UserItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './User.module.css';
+
+const UserItem = ({ user, index }) => {
+  return (
+    <tr className={styles.row}>
+      <td>{index + 1}</td>
+      <td className={styles.bold}>{user.name}</td>
+      <td>{user.email}</td>
+      <td>{user.phone}</td>
+      <td>{user.point.toLocaleString()} P</td>
+      <td>
+        <div className={styles.salesBox}>{user.sales} 건</div>
+      </td>
+      <td>{user.created_at}</td>
+    </tr>
+  );
+};
+
+export default UserItem;
