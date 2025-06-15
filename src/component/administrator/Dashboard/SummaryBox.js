451314c3d287698e99caaa10fd7451db3b0d9502
taeyeon0319 <xodus1349@likelion.org>
2025-06-15 15:40:47 +0900

    대시보드 디자인 수정

--- src/component/administrator/Dashboard/SummaryBox.js
+++ src/component/administrator/Dashboard/SummaryBox.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
-import SummaryItem from './SummaryItem';
 import styles from './Dashboard.module.css';
+import SummaryCard from './SummaryCard';
+import SummaryPFChart from './SummaryPFChart';
+import SummaryDefectChart from './SummaryDefectChart';
 
 const SummaryBox = () => {
   const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
-
-  const summaryData = [
-    { title: '검수대기', count: 129, change: { value: 16.3, up: true } },
-    { title: '출고대기', count: 87, change: { value: 5.2, up: false } },
-    { title: '반품대기', count: 45, change: { value: 3.1, up: true } },
-  ];
 
   return (
     <div className={styles.summaryBox}>
@@ -26,11 +22,10 @@ const SummaryBox = () => {
         />
       </div>
 
-      <div className={styles.items}>
-        {summaryData.map((item, idx) => (
-          <SummaryItem key={idx} {...item} />
-        ))}
-      {/* 수거 요청 들어온 대시보드 추가하기 */}
+      <div className={styles.grid}>
+        <SummaryCard/>
+        <SummaryPFChart/>
+        <SummaryDefectChart/>
       </div>
     </div>
   );
